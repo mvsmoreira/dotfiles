b8e141f6ac4d4518ca21cba241ee59662761fb22
Vinicius Moreira <mvs.moreira93@gmail.com>
2022-06-09 15:07:16 -0300

    feat: add open rgb

---
+++ src/post-install/open-rgb.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Add thopiekar repo. Available in https://launchpad.net/~thopiekar/+archive/ubuntu/openrgb
+sudo add-apt-repository ppa:thopiekar/openrgb
+
+# Install Openrgb
+sudo apt install openrgb -y
